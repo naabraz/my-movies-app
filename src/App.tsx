b3dbae1f89bc3dd400d6c5dc0f0d633c7aa31100
Natalia Braz <naabraz@gmail.com>
2020-03-21 20:49:36 -0300

    chore(react navigation): adding react navigation lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { SafeAreaView, ScrollView, View, Text, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <View>
-            <Text>My Movies App</Text>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <NavigationContainer>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView contentInsetAdjustmentBehavior="automatic">
+            <View>
+              <Text>My Movies App</Text>
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </NavigationContainer>
     </>
   );
 };
