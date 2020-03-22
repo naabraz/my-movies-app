695b4c4aefbdb5c76422cb5a613087799e0bb008
Natalia Braz <naabraz@gmail.com>
2020-03-21 21:07:30 -0300

    chore(react navigation): adding stack example

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, View, Text, StatusBar } from 'react-native';
+import { View, Text, StatusBar, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const HomeScreen = () => {
+  return (
+    <View style={style.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+const DetailsScreen = () => {
+  return (
+    <View style={style.container}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <>
       <NavigationContainer>
         <StatusBar barStyle="dark-content" />
-        <SafeAreaView>
-          <ScrollView contentInsetAdjustmentBehavior="automatic">
-            <View>
-              <Text>My Movies App</Text>
-            </View>
-          </ScrollView>
-        </SafeAreaView>
+        <Stack.Navigator initialRouteName="Details">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
       </NavigationContainer>
     </>
   );
