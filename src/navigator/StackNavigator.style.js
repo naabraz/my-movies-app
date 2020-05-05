0046b021090c926e9f9c067caf9843d193840de9
Natalia Braz <naabraz@gmail.com>
2020-05-05 20:52:37 -0300

    feat(header): adding style

---
+++ src/navigator/StackNavigator.style.js
@@ -0,0 +1,8 @@
+import * as colors from '../style/colors';
+
+export const screenOptions = {
+  headerStyle: {
+    backgroundColor: colors.gallery,
+  },
+  headerTintColor: colors.ebonyClay,
+};
--- src/navigator/StackNavigator.tsx
+++ src/navigator/StackNavigator.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { PopularMoviesContainer } from '../screens/';
+import * as Styles from './StackNavigator.style.js';
 
 const Stack = createStackNavigator();
 
 const StackNavigator: React.FC = () => (
-  <Stack.Navigator initialRouteName="Details">
+  <Stack.Navigator
+    initialRouteName="Popular Movies"
+    screenOptions={Styles.screenOptions}
+  >
     <Stack.Screen name="Popular Movies" component={PopularMoviesContainer} />
   </Stack.Navigator>
 );
