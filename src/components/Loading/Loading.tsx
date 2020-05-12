73abe4dd97768c7fb54040e3f453450aaf4c4da0
Natalia Braz <naabraz@gmail.com>
2020-05-12 19:35:44 -0300

    chore(assets folder): moving assets to src folder

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -5,7 +5,7 @@ import * as Styled from './Loading.style';
 export const Loading: React.FC = () => (
   <Styled.Container>
     <Styled.Animation
-      source={require('../../../assets/lottie/loading.json')}
+      source={require('../../assets/lottie/loading.json')}
       autoPlay
       loop
       hardwareAccelerationAndroid
