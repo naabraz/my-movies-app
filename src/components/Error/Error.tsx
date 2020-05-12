0f6891851496aafb08a29540d81d3daf9acaa005
Natalia Braz <naabraz@gmail.com>
2020-05-12 19:35:44 -0300

    chore(assets folder): moving assets to src folder

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -5,7 +5,7 @@ import * as Styled from './Error.style';
 export const Error: React.FC = () => (
   <Styled.Container>
     <Styled.Animation
-      source={require('../../../assets/lottie/error.json')}
+      source={require('../../assets/lottie/error.json')}
       autoPlay
       loop
       hardwareAccelerationAndroid
