4a717b02308cf43cf8acfd4e447e80df70698aa2
Natalia Braz <naabraz@gmail.com>
2020-05-14 20:10:52 -0300

    chore(unit test): adding unit test to apollo client

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'react-native',
-  testMatch: ['**/*.test.tsx'],
+  testMatch: ['**/*.test.tsx', '**/*.test.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverageFrom: [
     'src/**/*.{ts,tsx,js}',
