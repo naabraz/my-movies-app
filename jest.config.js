024318a5aedd39250bd613905449dd94f3900c22
Natalia Braz <naabraz@gmail.com>
2020-05-14 19:31:20 -0300

    chore(unit test): adjusting coverage and tests

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: 'react-native',
   testMatch: ['**/*.test.tsx'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/node_modules/**'],
 };
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import 'react-native';
 import renderer from 'react-test-renderer';
 
-import App from './App';
+import { App } from './App';
+
+jest.mock('@react-navigation/native', () => ({
+  NavigationContainer: jest
+    .fn()
+    .mockImplementation(() => 'NavigationContainer'),
+}));
+
+jest.mock('apollo', () => ({ apolloClient: jest.fn() }));
+jest.mock('./navigator', () => ({
+  StackNavigator: jest.fn().mockImplementation(() => 'StackNavigator'),
+}));
 
 it('renders correctly', () => {
   renderer.create(<App />);
