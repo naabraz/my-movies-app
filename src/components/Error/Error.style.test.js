d18e610370ff82f5106eab6414344d6500abcb1c
Natalia Braz <naabraz@gmail.com>
2020-05-19 18:24:13 -0300

    chore(unit test): removing unnecessary snapshot

--- src/components/Error/Error.style.test.js
+++ src/components/Error/Error.style.test.js
@@ -6,15 +6,7 @@ import * as Styled from './Error.style';
 describe('Given Error component style', () => {
   it('Should render Container component correctly', () => {
     const errorComponent = renderer.create(<Styled.Container />).toJSON();
-    expect(errorComponent).toMatchSnapshot();
     expect(errorComponent).toHaveStyleRule('justify-content', 'center');
     expect(errorComponent).toHaveStyleRule('align-self', 'center');
-  });
-
-  it('Should render Animation component correctly', () => {
-    const animationComponent = renderer
-      .create(<Styled.Animation source={{}} />)
-      .toJSON();
-    expect(animationComponent).toMatchSnapshot();
   });
 });
