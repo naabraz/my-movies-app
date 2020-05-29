d61c63f093486b6943ce12a1c301ec2aad7e83c0
Natalia Braz <naabraz@gmail.com>
2020-05-28 22:17:40 -0300

    feat(movie details): creating rating component

---
+++ src/screens/MovieDetails/components/Rating/Rating.style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex-direction: row;
+`;
+
+export const Rating = styled.Image`
+  width: 15px;
+  height: 15px;
+`;
---
+++ src/screens/MovieDetails/components/Rating/Rating.types.ts
@@ -0,0 +1,3 @@
+export type Rating = {
+  voteAverage: Float32Array;
+};
