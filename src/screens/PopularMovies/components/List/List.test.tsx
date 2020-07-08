ec9d94cb279da9ce69fb81f29147a632789eae03
Natalia Braz <naabraz@gmail.com>
2020-07-07 21:30:44 -0300

    chore(list): update unit test

--- src/screens/PopularMovies/components/List/List.test.tsx
+++ src/screens/PopularMovies/components/List/List.test.tsx
@@ -1,18 +1,36 @@
 import React from 'react';
-import { render } from 'react-native-testing-library';
+import { render, fireEvent } from 'react-native-testing-library';
+import { useNavigation } from '@react-navigation/native';
 
 import { PopularMoviesList } from './';
 
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: jest.fn().mockReturnValue({
+    navigate: jest.fn(),
+  }),
+}));
+
 describe('Given PopularMoviesList component', () => {
+  const movie = {
+    id: 1,
+    title: 'movieTitle',
+    posterPath: 'fooPath',
+    backdropPath: 'fooBackdropPath',
+  };
+
   it('Should render component correctly', () => {
-    const movie = {
-      id: 1,
-      title: 'movieTitle',
-      posterPath: 'fooPath',
-      backdropPath: 'fooBackdropPath',
-    };
     const { getAllByTestId } = render(<PopularMoviesList movie={movie} />);
 
     expect(getAllByTestId('MoviePoster')).toBeTruthy();
+  });
+
+  it('Should navigate to Movie Details screen when poster is clicked', () => {
+    const { getByTestId } = render(<PopularMoviesList movie={movie} />);
+
+    fireEvent.press(getByTestId('Button'));
+
+    expect(useNavigation().navigate).toHaveBeenCalledWith('Movie Details', {
+      movie,
+    });
   });
 });
