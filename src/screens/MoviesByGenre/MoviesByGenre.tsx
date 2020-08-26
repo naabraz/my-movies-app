2a49107f276fa76723079d0f5e2b8a178549cdf1
Natalia Braz <naabraz@gmail.com>
2020-08-25 22:44:29 -0300

    feat(movies by genre): create screen

---
+++ src/screens/MoviesByGenre/MoviesByGenre.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as Styled from './MoviesByGenre.style';
+
+export const MoviesByGenre: React.FC = () => {
+  return (
+    <Styled.Container>
+      <Styled.Text>Movies By Genre</Styled.Text>
+    </Styled.Container>
+  );
+};
---
+++ src/screens/MoviesByGenre/index.ts
@@ -0,0 +1,1 @@
+export { MoviesByGenre } from './MoviesByGenre';
