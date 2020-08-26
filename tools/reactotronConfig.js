ed97076de76dcea15a00349e24939f333c55f1a1
Natalia Braz <naabraz@gmail.com>
2020-08-25 22:07:22 -0300

    chore(gradle): updating gradle lib

--- tools/reactotronConfig.js
+++ tools/reactotronConfig.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from 'react-native';
+import { AsyncStorage } from '@react-native-community/async-storage';
 import Reactotron from 'reactotron-react-native';
 
 const reactotronConfig = () => {
