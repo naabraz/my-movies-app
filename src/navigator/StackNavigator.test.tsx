8744fb5bc4ab296b13f9c3b7abb0eb400c5a6cbc
Natalia Braz <naabraz@gmail.com>
2020-12-07 20:12:31 -0300

    chore(stack navigator): adjust unit test

--- src/navigator/StackNavigator.test.tsx
+++ src/navigator/StackNavigator.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { render, act } from '@testing-library/react-native';
+import { render, waitFor } from '@testing-library/react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import wait from 'waait';
 
-import { StackNavigator } from './';
+import { StackNavigator } from './index';
 
+jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
 jest.mock('screens', () => ({
   PopularMovies: 'PopularMovies',
   MovieDetails: 'MovieDetails',
@@ -19,8 +19,8 @@ describe('Given navigator component', () => {
       </NavigationContainer>,
     );
 
-    await act(() => wait(0));
+    const popularMovies = await waitFor(() => getByText('Popular Movies'));
 
-    expect(getByText('Popular Movies')).toBeTruthy();
+    expect(popularMovies).toBeTruthy();
   });
 });
