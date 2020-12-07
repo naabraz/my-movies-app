ea3d9d6dbc079fa7015b64940b373e53b236757c
Natalia Braz <naabraz@gmail.com>
2020-12-07 20:26:05 -0300

    chore(movies list component): adjust unit test

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -11,7 +11,7 @@ export const MoviesList: React.FC<Movie> = ({ movie }: Movie) => {
   const goToDetailsScreen = (): void => navigate('Movie Details', { movie });
 
   return (
-    <Styled.Button onPress={goToDetailsScreen} testID="Button">
+    <Styled.Button onPress={goToDetailsScreen}>
       <Styled.Container>
         <Styled.Poster
           source={{ uri: backdropPath }}
