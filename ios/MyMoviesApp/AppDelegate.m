c8b95c9db276c478095680d5441cfd7b3e32f453
Natalia Braz <naabraz@gmail.com>
2021-07-15 23:08:09 -0300

    feat: add firebase to ios project

--- ios/MyMoviesApp/AppDelegate.m
+++ ios/MyMoviesApp/AppDelegate.m
@@ -11,6 +11,9 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+
+#import <Firebase.h>
+
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
   SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
@@ -26,6 +29,10 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
+  
   #ifdef FB_SONARKIT_ENABLED
     InitializeFlipper(application);
   #endif
