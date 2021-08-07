51a7fe81d0025bab6ef2978d7ba66c264717a1f4
Natalia Braz <naabraz@gmail.com>
2021-08-06 22:51:06 -0300

    feat: add device info

--- src/screens/PopularMovies/index.tsx
+++ src/screens/PopularMovies/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
+import { getBuildNumber } from 'react-native-device-info';
 
 import { Loading, Error, MoviesList } from 'components';
 import { POPULAR_MOVIES } from './index.graphql';
 import { Movies as MoviesType } from './types';
-import { Container, Movies } from './styles';
+import { Container, Movies, Version } from './styles';
 
 const PopularMovies: React.FC = () => {
   const { loading, data, error } = useQuery<MoviesType>(POPULAR_MOVIES);
@@ -20,6 +21,7 @@ const PopularMovies: React.FC = () => {
   return (
     <Container>
       <Movies>{moviesList}</Movies>
+      <Version>App Version: {getBuildNumber()}</Version>
     </Container>
   );
 };
--- src/screens/PopularMovies/styles.js
+++ src/screens/PopularMovies/styles.js
@@ -4,10 +4,20 @@ import * as colors from 'style/colors';
 
 export const Container = styled.ScrollView`
   background-color: ${colors.gallery};
+  margin-horizontal: 16px;
+  margin-vertical: 16px;
 `;
 
 export const Movies = styled.View`
   flex-wrap: wrap;
   flex-direction: row;
   justify-content: center;
+`;
+
+export const Version = styled.Text`
+  margin-top: 12px;
+  font-size: 8px;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: ${colors.ebonyClay};
 `;
