9598021fb7e1fd3b2f153dd73dc66da14fea60bf
Natalia Braz <naabraz@gmail.com>
2021-09-14 17:07:50 -0300

    feat: secure storage module

---
+++ ios/NativeModules/SecureStorage/SecureStorage.m
@@ -0,0 +1,19 @@
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(SecureStorage, NSObject)
+
+RCT_EXTERN_METHOD(getValue:(NSString *) account
+                  resolver: (RCTPromiseResolveBlock) resolve
+                  rejecter: (RCTPromiseRejectBlock) reject)
+
+RCT_EXTERN_METHOD(setValue:(NSString *) account
+                  value: (NSString *) value
+                  resolver: (RCTPromiseResolveBlock) resolve
+                  rejecter: (RCTPromiseRejectBlock) reject)
+
+RCT_EXTERN_METHOD(deleteValue:(NSString *) account
+                  resolver: (RCTPromiseResolveBlock) resolve
+                  rejecter: (RCTPromiseRejectBlock) reject)
+
+@end
---
+++ ios/NativeModules/SecureStorage/SecureStorage.swift
@@ -0,0 +1,63 @@
+import Foundation
+import React
+
+@objc(SecureStorage)
+class SecureStorage: NSObject {
+  
+  let bundleID = Bundle.main.bundleIdentifier
+  
+  @objc(getValue:resolver:rejecter:)
+  func getValue(account: String,
+                resolver resolve: RCTPromiseResolveBlock,
+                rejecter reject: RCTPromiseRejectBlock) -> Void {
+    
+    do {
+      let value = try getValueFromKeychain(account: account,
+                                           service: bundleID!)
+      
+      resolve(value)
+    } catch KeychainError.itemNotFound {
+      reject("SecureStorage", "getValue: Item not found", nil);
+    } catch KeychainError.unexpectedValueData {
+      reject("SecureStorage", "getValue: Unexpected value data", nil);
+    } catch KeychainError.unhandledError {
+      reject("SecureStorage", "getValue: unhandledError", nil)
+    } catch {
+      reject("SecureStorage", "getValue: Generic error", nil);
+    }
+  }
+  
+  @objc(setValue:value:resolver:rejecter:)
+  func setValue(account: String,
+                value: String,
+                resolver resolve: RCTPromiseResolveBlock,
+                rejecter reject: RCTPromiseRejectBlock) -> Void {
+    
+    do {
+      try setValueOnKeychain(account: account,
+                             service: bundleID!,
+                             value: value)
+      resolve("Success")
+    } catch KeychainError.unhandledError {
+      reject("SecureStorage", "setValue: unhandledError", nil)
+    } catch {
+      reject("SecureStorage", "setValue: Generic error", nil);
+    }
+  }
+  
+  @objc(deleteValue:resolver:rejecter:)
+  func deleteValue(account: String,
+                   resolver resolve: RCTPromiseResolveBlock,
+                   rejecter reject: RCTPromiseRejectBlock) -> Void {
+    
+    do {
+      try deleteValueFromKeychain(account: account,
+                                  service: bundleID!)
+      resolve("Success")
+    } catch KeychainError.unhandledError {
+      reject("SecureStorage", "deleteValue: unhandledError", nil)
+    } catch {
+      reject("SecureStorage", "deleteValue: Generic error", nil);
+    }
+  }
+}
