d578595489af7a303442a7046f8fc5cd3447537d
Natalia Braz <naabraz@gmail.com>
2021-09-22 15:45:34 -0300

    feat: create apollo client

---
+++ ios/ApolloClient/NetworkInterceptorProvider.swift
@@ -0,0 +1,29 @@
+import Foundation
+import Apollo
+
+struct NetworkInterceptorProvider: InterceptorProvider {
+  
+  private let store: ApolloStore
+  private let client: URLSessionClient
+  
+  init(store: ApolloStore,
+       client: URLSessionClient) {
+    self.store = store
+    self.client = client
+  }
+  
+  func interceptors<Operation: GraphQLOperation>(for operation: Operation) -> [ApolloInterceptor] {
+    return [
+      MaxRetryInterceptor(),
+      LegacyCacheReadInterceptor(store: self.store),
+      UserManagementInterceptor(),
+      RequestLoggingInterceptor(),
+      NetworkFetchInterceptor(client: self.client),
+      //            ResponseLoggingInterceptor(),
+      ResponseCodeInterceptor(),
+      LegacyParsingInterceptor(cacheKeyForObject: self.store.cacheKeyForObject),
+      AutomaticPersistedQueryInterceptor(),
+      LegacyCacheWriteInterceptor(store: self.store)
+    ]
+  }
+}
---
+++ ios/ApolloClient/RequestLoggingInterceptor.swift
@@ -0,0 +1,17 @@
+import Foundation
+import Apollo
+
+class RequestLoggingInterceptor: ApolloInterceptor {
+  
+  func interceptAsync<Operation: GraphQLOperation>(
+    chain: RequestChain,
+    request: HTTPRequest<Operation>,
+    response: HTTPResponse<Operation>?,
+    completion: @escaping (Result<GraphQLResult<Operation.Data>, Error>) -> Void) {
+      
+      print("Outgoing request: \(request)")
+      chain.proceedAsync(request: request,
+                         response: response,
+                         completion: completion)
+    }
+}
