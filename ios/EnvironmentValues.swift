e074445a09f8421e2e207f2d79e900b5bcb46a8a
Natalia Braz <naabraz@gmail.com>
2021-09-28 18:21:13 -0300

    chore: remove env file

--- ios/EnvironmentValues.swift
+++
@@ -1,8 +0,0 @@
-// Generated using Sourcery 1.5.0 — https://github.com/krzysztofzablocki/Sourcery
-// DO NOT EDIT
-
-public struct EnvironmentValues {
-	let apiURL: String
-}
-
-public let environmentValues = EnvironmentValues(apiURL: "")
