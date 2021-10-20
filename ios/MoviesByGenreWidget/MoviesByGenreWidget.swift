211c229f827de8ae26408ca67ff9d6776ef75137
Natalia Braz <naabraz@gmail.com>
2021-10-20 00:08:19 -0300

    chore: render graphql movie genre

--- ios/MoviesByGenreWidget/MoviesByGenreWidget.swift
+++ ios/MoviesByGenreWidget/MoviesByGenreWidget.swift
@@ -3,6 +3,7 @@ import SwiftUI
 
 struct MoviesByGenreWidgetEntry: TimelineEntry {
   let date: Date
+  let genreName: String
   let genreMovie: String
 }
 
@@ -10,39 +11,77 @@ let genre: Genre = getFavoriteGenre()
 
 struct MoviesByGenreWidgetProvider: TimelineProvider {
   func placeholder(in context: Context) -> MoviesByGenreWidgetEntry {
-    MoviesByGenreWidgetEntry(date: Date(), genreMovie: getMoviesByGenreMock(genreId: genre.id))
+    MoviesByGenreWidgetEntry(
+      date: Date(),
+      genreName: "Placeholder Genre",
+      genreMovie: "Placeholder Movie"
+    )
   }
   
   func getSnapshot(in context: Context, completion: @escaping (MoviesByGenreWidgetEntry) -> ()) {
-    let entry = MoviesByGenreWidgetEntry(date: Date(), genreMovie: getMoviesByGenreMock(genreId: genre.id))
+    let entry = MoviesByGenreWidgetEntry(
+      date: Date(),
+      genreName: "Snapshot Genre",
+      genreMovie: "Snapshot Movie"
+    )
+    
     completion(entry)
   }
   
   func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
     var entries: [MoviesByGenreWidgetEntry] = []
     
-    // Generate a timeline consisting of five entries an hour apart, starting from the current date.
     let currentDate = Date()
     for hourOffset in 0 ..< 5 {
-      let entryDate = Calendar.current.date(byAdding: .minute, value: hourOffset, to: currentDate)!
-      let entry = MoviesByGenreWidgetEntry(date: entryDate, genreMovie: getMoviesByGenreMock(genreId: genre.id))
-      entries.append(entry)
+      let entryDate = Calendar.current.date(
+        byAdding: .minute,
+        value: hourOffset,
+        to: currentDate
+      )!
+      
+      Network.shared.apollo.fetch(query: MoviesByGenreQuery(genreId: genre.id)) { result in
+        switch result {
+        case .success(let graphQLResult):
+          if let result = graphQLResult.data?.moviesByGenre {
+            let movies = result.compactMap{$0}
+            
+            let entry = MoviesByGenreWidgetEntry(
+              date: entryDate,
+              genreName: genre.name,
+              genreMovie: (movies.first?.title!)!
+            )
+            
+            let timeline = Timeline(entries: [entry], policy: .atEnd)
+            
+            entries.append(entry)
+            completion(timeline)
+          }
+        case .failure(let error):
+          print("Failure! Error: \(error)")
+          
+          let entry = MoviesByGenreWidgetEntry(
+            date: entryDate,
+            genreName: genre.name,
+            genreMovie: "Failure"
+          )
+          
+          let timeline = Timeline(entries: [entry], policy: .atEnd)
+          
+          entries.append(entry)
+          completion(timeline)
+        }
+      }
     }
-    
-    let timeline = Timeline(entries: entries, policy: .atEnd)
-    completion(timeline)
   }
 }
 
 struct MoviesByGenreWidgetEntryView : View {
   var entry: MoviesByGenreWidgetProvider.Entry
-
+  
   var body: some View {
     VStack {
-      Text("Genre: \(genre.name)")
-      Text("Movie: \(entry.genreMovie)")
-    }.onAppear {
-      getMoviesByGenre(genreId: genre.id)
+      Text(entry.genreName)
+      Text(entry.genreMovie)
     }
   }
 }
