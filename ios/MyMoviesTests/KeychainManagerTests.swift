bbefb734d4af10060d6c0a5af122485f106b657b
Natalia Braz <naabraz@gmail.com>
2021-10-28 07:33:04 -0300

    chore: add keychain manager tests

---
+++ ios/MyMoviesTests/KeychainManagerTests.swift
@@ -0,0 +1,37 @@
+import XCTest
+
+@testable import My_Movies
+
+class KeychainManagerTests: XCTestCase {
+  let mockKeychain = MockKeychain()
+  lazy var mockKeychainManager = KeychainManager(keychain: mockKeychain)
+  
+  override func tearDown() {
+    super.tearDown()
+    mockKeychain.clearData()
+  }
+  
+  func testSaveValue_whenItemExist_valueUpdated() {
+    mockKeychain.keychainResult = KeychainResult(
+      status: noErr,
+      queryResult: [kSecValueData: "12345".data(using: .utf8)] as AnyObject
+    )
+
+    let item = GenericPasswordItem(service: "",
+                                   account: nil,
+                                   accessGroup: nil)
+    
+    XCTAssertNoThrow(try mockKeychainManager.saveValue("10", to: item))
+    XCTAssertEqual((mockKeychain.attributesToUpdate[kSecValueData as String] as? Data),
+                   "10".data(using: String.Encoding.utf8)
+    )
+  }
+  
+  func testDeleteItem_whenItemNoExist_deleteItemSucceeded() {
+    mockKeychain.osStatus = errSecItemNotFound
+    
+    let item = GenericPasswordItem(service: "", account: nil, accessGroup: nil)
+    
+    XCTAssertNoThrow(try mockKeychainManager.deleteItem(item))
+  }
+}
