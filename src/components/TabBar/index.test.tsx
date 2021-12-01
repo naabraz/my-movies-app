b77a36eebfa8c9e056969e7191cf2caf194a18b8
Natalia Braz <naabraz@gmail.com>
2021-11-30 22:12:38 -0300

    test: add tab bar unit test

---
+++ src/components/TabBar/index.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import { useNavigation } from '@react-navigation/native';
+
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: jest.fn().mockReturnValue({
+    navigate: jest.fn(),
+  }),
+}));
+
+import TabBar from './index';
+
+const tabBarProps = {
+  state: {
+    routes: [
+      {
+        name: 'Popular Movies',
+      },
+      {
+        name: 'Favorite Genres',
+      },
+    ],
+    index: 0,
+  },
+};
+
+test('should render tab bar with routes', () => {
+  const { getByText } = render(<TabBar {...tabBarProps} />);
+
+  expect(getByText('Popular Movies')).toBeTruthy();
+  expect(getByText('Favorite Genres')).toBeTruthy();
+});
+
+test('should navigate to route item is pressed', () => {
+  const { getByText } = render(<TabBar {...tabBarProps} />);
+
+  fireEvent.press(getByText('Popular Movies'));
+
+  expect(useNavigation().navigate).toHaveBeenCalledWith({
+    name: 'Popular Movies',
+  });
+});
