ca08bd2dd7f8524f5ff619f6d9d854f99daa93bb
Natalia Braz <naabraz@gmail.com>
2021-11-30 21:42:00 -0300

    chore: adjust jest config

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,12 @@ module.exports = {
   testMatch: ['**/*.test.tsx', '**/*.test.ts', '**/*.test.js'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native|@react-native-community|@react-navigation)',
+    '<rootDir>/node_modules/(?!(jest-)?react-native|@gympass/*|@react-navigation/.*|@react-native|@react-native-community|@react-navigation|@react-native-picker/picker)/',
   ],
+  moduleNameMapper: {
+    'styled-components':
+      '<rootDir>/node_modules/styled-components/native/dist/styled-components.native.cjs.js',
+  },
   collectCoverageFrom: [
     'src/**/*.{ts,tsx,js}',
     '!**/node_modules/**',
