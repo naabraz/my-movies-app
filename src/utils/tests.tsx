87c3fceedf68884c1f3038b3bec3b43280561df3
Natalia Braz <naabraz@gmail.com>
2022-01-11 20:22:43 -0300

    chore(test): create wrapper component

---
+++ src/utils/tests.tsx
@@ -0,0 +1,19 @@
+import React, { FC, ReactElement } from 'react';
+import {
+  render,
+  RenderAPI,
+  RenderOptions,
+} from '@testing-library/react-native';
+import { ThemeProvider } from '@gympass/yoga';
+
+const ComponentWithProviders: FC = ({ children }) => {
+  return <ThemeProvider>{children}</ThemeProvider>;
+};
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+): RenderAPI => render(ui, { wrapper: ComponentWithProviders, ...options });
+
+export * from '@testing-library/react-native';
+export { customRender as render };
