90e16c0f80f8a5516661334eaa6baf16d11230d0
Natalia Braz <naabraz@gmail.com>
2022-01-11 20:23:11 -0300

    test(tab bar): update to use wrapper render

--- src/components/TabBar/index.test.tsx
+++ src/components/TabBar/index.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
+import { fireEvent, RenderAPI } from '@testing-library/react-native';
 import { useNavigation } from '@react-navigation/native';
+import { render } from 'src/utils/tests';
 
 jest.mock('@react-navigation/native', () => ({
   useNavigation: jest.fn().mockReturnValue({
@@ -24,15 +25,17 @@ const tabBarProps = {
   },
 };
 
+const setup = (): RenderAPI => render(<TabBar {...tabBarProps} />);
+
 test('should render tab bar with routes', () => {
-  const { getByText } = render(<TabBar {...tabBarProps} />);
+  const { getByText } = setup();
 
   expect(getByText('Popular Movies')).toBeTruthy();
   expect(getByText('Favorite Genre')).toBeTruthy();
 });
 
 test('should navigate to route item is pressed', () => {
-  const { getByText } = render(<TabBar {...tabBarProps} />);
+  const { getByText } = setup();
 
   fireEvent.press(getByText('Popular Movies'));
 
