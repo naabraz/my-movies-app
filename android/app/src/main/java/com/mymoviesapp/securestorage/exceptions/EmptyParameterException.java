966be4e803f54d7520cd95714d0d42a1ea0be8b4
Natalia Braz <naabraz@gmail.com>
2022-01-20 21:56:00 -0300

    chore(android secure storage): improve module

---
+++ android/app/src/main/java/com/mymoviesapp/securestorage/exceptions/EmptyParameterException.java
@@ -0,0 +1,7 @@
+package com.mymoviesapp.securestorage.exceptions;
+
+public class EmptyParameterException extends Exception {
+    public EmptyParameterException(String message) {
+        super(message);
+    }
+}
